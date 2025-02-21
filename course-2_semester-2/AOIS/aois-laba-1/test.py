592bf7badf8fc777c8635114e144cadc3a8f1d64
KuKriK <ilapolityko@gmail.com>
2025-02-21 19:07:00 +0300

    Update AOIS

---
+++ course-2_semester-2/AOIS/aois-laba-1/test.py
@@ -0,0 +1,57 @@
+import unittest
+from unittest.mock import patch
+from main import (menu,
+    binary_to_decimal, addition_bin, to_bin,
+    multiplication_bin, division_bin, float_to_ieee754, ieee754_to_float, addition_float
+)
+
+class MyTestCase(unittest.TestCase):
+    def test_binary_to_decimal(self):
+        self.assertEqual(binary_to_decimal([0, 1, 0, 1]), 5)
+        self.assertEqual(binary_to_decimal([1, 1, 0, 1]), -5)
+
+    def test_addition_bin(self):
+        self.assertEqual(addition_bin([0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 1, 0]), [0, 0, 0, 0, 0, 1, 0, 1])
+
+    def test_to_bin(self):
+        self.assertEqual(to_bin(5, 'direct'), [0, 0, 0, 0, 0, 1, 0, 1])
+        self.assertEqual(to_bin(-5, 'additionally'), [1, 1, 1, 1, 1, 0, 1, 1])
+
+    def test_multiplication_bin(self):
+        self.assertEqual(multiplication_bin([0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 1, 0]), [0, 0, 0, 0, 0, 1, 1, 0])
+
+    def test_division_bin(self):
+        self.assertEqual(division_bin([0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 1, 0]), '1.50000')
+
+    def test_float_to_ieee754(self):
+        ieee = float_to_ieee754(3.5)
+        self.assertEqual(ieee[:9], [0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_ieee754_to_float(self):
+        self.assertAlmostEqual(ieee754_to_float(float_to_ieee754(3.5)), 3.5, places=5)
+
+    def test_addition_float(self):
+        ieee_sum = addition_float(3.5, 2.5)
+        self.assertAlmostEqual(ieee754_to_float(ieee_sum), 6.0, places=5)
+
+    def test_menu_addition(self, mock_input):
+        result = menu(1)
+        self.assertEqual(result, 8)
+
+    @patch('builtins.input', side_effect=['5', '3'])
+    def test_menu_subtraction(self, mock_input):
+        result = menu(2)
+        self.assertEqual(result, 2)
+
+    @patch('builtins.input', side_effect=['5', '3'])
+    def test_menu_multiplication(self, mock_input):
+        result = menu(3)
+        self.assertEqual(result, 15)
+
+    @patch('builtins.input', side_effect=['6', '3'])
+    def test_menu_division(self, mock_input):
+        result = menu(4)
+        self.assertEqual(result, '2.00000')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ course-2_semester-2/AOIS/aois-laba-2/main.py
@@ -0,0 +1,189 @@
+import math
+import string
+
+
+def process_expression(expression):
+    def get_operator_priority(operator):
+        priorities = {
+            '!': 4,
+            '&': 3,
+            '|': 2,
+            '->': 1,
+            '~': 1,
+            '(': 0
+        }
+        return priorities.get(operator, 0)
+
+    def infix_to_rpn(expr):
+        output = []
+        operator_stack = []
+        i = 0
+
+        while i < len(expr):
+            if expr[i].isspace():
+                i += 1
+                continue
+
+            if expr[i] in 'abcde':
+                output.append(expr[i])
+                i += 1
+                continue
+
+            if expr[i] == '!' and i + 1 < len(expr) and expr[i + 1] in 'abcde':
+                output.append(f"!{expr[i + 1]}")
+                i += 2
+                continue
+
+            if expr[i] == '(':
+                operator_stack.append('(')
+                i += 1
+                continue
+
+            if expr[i] == ')':
+                while operator_stack and operator_stack[-1] != '(':
+                    output.append(operator_stack.pop())
+                if operator_stack and operator_stack[-1] == '(':
+                    operator_stack.pop()
+                i += 1
+                continue
+
+            if expr[i] in '&|!->~':
+                current_op = expr[i]
+                if current_op == '-' and i + 1 < len(expr) and expr[i + 1] == '>':
+                    current_op = '->'
+                    i += 1
+
+                while (operator_stack and operator_stack[-1] != '(' and
+                       get_operator_priority(operator_stack[-1]) >= get_operator_priority(current_op)):
+                    output.append(operator_stack.pop())
+
+                operator_stack.append(current_op)
+                i += 1
+                continue
+
+            i += 1
+
+        while operator_stack:
+            if operator_stack[-1] != '(':
+                output.append(operator_stack.pop())
+            else:
+                operator_stack.pop()
+
+        return output
+
+    rpn_tokens = infix_to_rpn(expression)
+    vars = [x for x in rpn_tokens if x[0] in 'abcde' or (len(x) > 1 and x[0] == '!' and x[1] in 'abcde')]
+
+    return vars, rpn_tokens
+
+
+def to_bin(num, length):
+    direct = []
+    while num != 0:
+        direct.insert(-1, num % 2)
+        num = math.trunc(num / 2)
+    direct.reverse()
+    if len(direct) < length:
+        direct = direct[:1] + [0] * (length - len(direct)) + direct[1:]
+    return direct
+
+
+def make_operation(first: int, second: int, operation: string):
+    if operation == '&':
+        if first == 1 and second == 1:
+            return 1
+        return 0
+    elif operation == '|':
+        if first == 0 and second == 0:
+            return 0
+        return 1
+    elif operation == '->':
+        if first == 1 and second == 0:
+            return 0
+        return 1
+    elif operation == '~':
+        if first == second:
+            return 1
+        return 0
+    else:
+        return 0
+
+
+def rpn_check(rpn):
+    vars: [string] = []
+    for element in range(len(rpn)):
+        if rpn[element] == 0 or rpn[element] == 1:
+            vars.append(rpn[element])
+            continue
+        el: int = make_operation(vars[-1], vars[-2], rpn[element])
+        vars.pop()
+        vars.pop()
+        vars.append(el)
+    return vars[0]
+
+
+def table_create(exp: string, rpn: [], vars: []):
+    vars_in_degree: int = 2 ** len(vars)
+    vars_string: string = ''
+    for i in vars:
+        vars_string += i + ' '
+    vars_string += '' + exp
+    print(vars_string)
+    index_form: [int] = []
+    pdnf_exp_list: str = ''
+    pcnf_exp_list: str = ''
+    pdnf_list: [int] = []
+    pcnf_list: [int] = []
+    for i in range(vars_in_degree):
+        bool_vars = to_bin(i, len(vars))
+        str_numbers = [str(n) for n in bool_vars]
+        bool_rpn: [string] = rpn.copy()
+        for f in range(len(vars)):
+            index = bool_rpn.index(vars[f])
+            if '!' in vars[f]:
+                if (bool_vars[f] == 0):
+                    bool_rpn[index] = 1
+                else:
+                    bool_rpn[index] = 0
+            else:
+                bool_rpn[index] = bool_vars[f]
+        check_info = rpn_check(bool_rpn)
+        index_form.append(check_info)
+        bool_vars_string = ' '.join(str_numbers) + '  ' + str(check_info)
+        result_exp = exp
+        for f in range(len(vars)):
+            if '!' in vars[f] and bool_vars[f] == 0:
+                continue
+            elif '!' in vars[f] and bool_vars[f] == 1:
+                result_exp = result_exp.replace(vars[f], vars[f][1])
+            elif bool_vars[f] == 0:
+                result_exp = result_exp.replace(vars[f], '!' + vars[f])
+        if check_info == 1:
+            pdnf_list.append(i)
+            pdnf_exp_list += '(' + result_exp + ')∨'
+
+        else:
+            pcnf_list.append(i)
+            pcnf_exp_list += '(' + result_exp + ')∨'
+        # print(bool_rpn)
+        print(bool_vars_string)
+    if len(pdnf_exp_list) != 0:
+        pdnf_exp_list = pdnf_exp_list[:-1]
+    if len(pcnf_exp_list) != 0:
+        pcnf_exp_list = pcnf_exp_list[:-1]
+    return {'index': index_form, 'pdnf': pdnf_list, 'pcnf': pcnf_list, 'pdnf_exp_list': pdnf_exp_list,
+            'pcnf_exp_list': pcnf_exp_list}
+
+
+if __name__ == '__main__':
+    expr = str(input("Введите логическую функцию:"))
+    vars, rpn = process_expression(expr)
+    print(f"\nВыражение: {expr}")
+    print("Переменные:", vars)
+    print("RPN:", rpn),
+    table_info = table_create(expr, rpn, vars)
+    print(f"Индексная форма: {table_info['index']}")
+    print(f"Числовая форма СДНФ: {table_info['pdnf']}")
+    print(f"Числовая форма СКНФ: {table_info['pcnf']}")
+    print(f"Совершенная дизъюнктивная нормальная форма (СДНФ): {table_info['pdnf_exp_list']}")
+    print(f"Совершенная конъюнктивная нормальная форма (СKНФ): {table_info['pcnf_exp_list']}")
