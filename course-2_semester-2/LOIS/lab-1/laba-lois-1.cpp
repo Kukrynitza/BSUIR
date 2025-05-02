15f8f05001e35982ee45fa0341d6c14dc90d7363
KuKriK <ilapolityko@gmail.com>
2025-05-02 23:59:20 +0300

    Update LOIS lab-1

--- course-2_semester-2/LOIS/lab-1/laba-lois-1.cpp
+++ course-2_semester-2/LOIS/lab-1/laba-lois-1.cpp
@@ -1,6 +1,6 @@
 ﻿// Вариант: F14
 //﻿ Лабораторная работа №1 по дисциплине Логические Основы Интеллектуальных Систем
-// Выполнена студентом группы 321701:Политыко Ильей Андреевичем
+// Выполнена студентом группы 321701: Политыко Ильей Андреевичем
 //
 // 01.05.2025
 //
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <cctype>
 
 using namespace std;
@@ -34,6 +35,7 @@ bool isValidClause(const string& clause) {
     size_t i = 0;
     string token;
     bool expectLiteral = true;
+    unordered_set<string> literals;
 
     while (i < clause.size()) {
         char ch = clause[i];
@@ -58,12 +60,19 @@ bool isValidClause(const string& clause) {
             else if (isalpha(ch)) {
                 token += ch;
                 ++i;
+                if (i < clause.size() && isalpha(clause[i])) return false;
             }
             else {
                 return false;
             }
 
             if (!isValidLiteral(token)) return false;
+
+            if (literals.count(token)) return false;
+            string negated = (token[0] == '!') ? string(1, token[1]) : ("!" + token);
+            if (literals.count(negated)) return false;
+
+            literals.insert(token);
             expectLiteral = false;
         }
     }
@@ -74,6 +83,7 @@ bool isCNF(const string& formula) {
     size_t i = 0;
     vector<string> clauses;
     string clause;
+    bool expectClause = true;
 
     while (i < formula.size()) {
         if (isspace(formula[i])) {
@@ -82,6 +92,7 @@ bool isCNF(const string& formula) {
         }
 
         if (formula[i] == '(') {
+            if (!expectClause) return false;
             ++i;
             clause.clear();
             int parenCount = 1;
@@ -98,16 +109,19 @@ bool isCNF(const string& formula) {
 
             if (parenCount != 0 || !isValidClause(clause)) return false;
             clauses.push_back(clause);
+            expectClause = false;
         }
         else if (formula[i] == '&') {
+            if (expectClause) return false;
             ++i;
+            expectClause = true;
         }
         else {
             return false;
         }
     }
 
-    return !clauses.empty();
+    return !clauses.empty() && !expectClause;
 }
 
 string readFormulaFromFile(const string& filename) {
@@ -123,10 +137,61 @@ string readFormulaFromFile(const string& filename) {
     return formula;
 }
 
+void userTest() {
+    vector<pair<string, bool>> testCases = {
+    {"(A|B)&(C|!D)", true},
+    {"(A|!B)&(C|D|!E)&(V|!Z)&&(A|S|!V)", false},
+    {"(Z|!Z)", false},
+    {"A|B", false},
+    {"((A|B)&(C))", false},
+    {"(A|B|A)", false},
+    {"(A|B)&(C|D)", true},
+    {"(A|!B)&(C|D|!E)", true},
+    {"&(A|B)", false},
+    {"(A|B)&", false},
+    {"(A|1)", false},
+    {"(A|!b)&(B|!a)", true},
+    {"(A|!B)&(C|!B)&(!A|C)", true},
+    {"(X|Y|Z)&(!X|!Y|!Z)", true},
+    {"(A|!A)", false},
+    {"()", false},
+    {"(AB)", false},
+    {"((A))", false},
+    };
+
+    int correct = 0;
+    for (size_t i = 0; i < testCases.size(); ++i) {
+        cout << "\nФормула #" << i + 1 << ": " << testCases[i].first << "\n";
+        cout << "Это КНФ? (1 - да, 0 - нет): ";
+        string answer;
+        getline(cin, answer);
+        if (answer != "0" && answer != "1") {
+            cout << "Некорректный ввод. Пропускаем.\n";
+            continue;
+        }
+
+        bool userAnswer = (answer == "1");
+        if (userAnswer == testCases[i].second) {
+            cout << "Правильно!\n";
+            ++correct;
+        }
+        else {
+            cout << "Неправильно. ";
+            cout << "Правильный ответ: " << (testCases[i].second ? "КНФ" : "не КНФ") << "\n";
+        }
+    }
+
+    cout << "\nВы ответили правильно на " << correct << " из " << testCases.size() << " вопросов.\n";
+}
+
 int main() {
     system("chcp 1251");
     string input;
-    cout << "Выберите источник формулы:\n1 - Ввод с консоли\n2 - Чтение из файла\n3 - Автор\n> ";
+    cout << "Выберите источник формулы:\n";
+    cout << "1 - Ввод с консоли\n";
+    cout << "2 - Чтение из файла\n";
+    cout << "3 - Пройти тест на знание КНФ\n";
+    cout << "4 - Автор\n";
     int choice;
     cin >> choice;
     cin.ignore();
@@ -142,7 +207,11 @@ int main() {
         input = readFormulaFromFile(filename);
     }
     else if (choice == 3) {
-        cout << "Политыко Илья Андреевич";
+        userTest();
+        return 1;
+    }
+    else if (choice == 4) {
+        cout << "Политыко Илья Андреевич\n";
         return 1;
     }
     else {
@@ -164,3 +233,4 @@ int main() {
 
     return 0;
 }
+
